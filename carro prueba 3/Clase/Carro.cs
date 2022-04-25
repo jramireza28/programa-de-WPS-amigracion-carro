d9dc11efa7f58cce6a396929821c56c99601868e
jramireza28 <jairo1234*>
2022-04-25 00:06:30 -0600

    first commit

---
+++ carro prueba 3/Clase/Carro.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace carro_prueba_3.Clase
+{
+    
+    
+        public class Carro
+        {
+            public string Marca { get; }
+            public int Modelo { get; }
+
+            public String Color { get; set; }
+
+            private int Encendido = 0;
+            private int velocidad_actual = 0;
+            //private const int MAXVELOCIDAD = 250;
+            public int MAXVELOCIDAD { get;  }
+
+
+        public Carro(string _marca, int _modelo, string _color, int _maxvel)
+            {
+                Marca = _marca;
+                Modelo = _modelo;
+                Color = _color;
+                Encendido = 0;
+            MAXVELOCIDAD = _maxvel;
+                this.velocidad_actual = 0;
+            }
+
+
+
+            public string acelerar()
+            {
+                if (Encendido == 0)
+                {
+                    return $"El carro esta apagado, asi no se puede";
+                }
+
+
+                string mensaje = "";
+                if (velocidad_actual <= MAXVELOCIDAD)
+                {
+                    velocidad_actual += 20;
+                    mensaje = $" {velocidad_actual} KPH";
+                }
+                else
+                {
+                    velocidad_actual = MAXVELOCIDAD;
+                    mensaje = "Vas muy Rapido!!";
+                }
+
+                Console.WriteLine(mensaje);
+                return mensaje;
+
+            }
+
+
+
+            public string acelerar(int AcekerarAkph)
+            {
+                if (Encendido == 0)
+                {
+                    return $"El carro esta apagado, asi no se puede";
+                }
+
+
+                string mensaje = "";
+                if (velocidad_actual <= MAXVELOCIDAD)
+                {
+                    velocidad_actual += AcekerarAkph;
+                    mensaje = $"vas a {velocidad_actual} KPH";
+                }
+                else
+                {
+                    velocidad_actual = MAXVELOCIDAD;
+                    mensaje = "Exceso de Velocidad!!";
+                }
+
+                Console.WriteLine(mensaje);
+                return mensaje;
+
+            }
+
+
+
+            public string GetVelocidadActual()
+            {
+                return $"VAmos a {velocidad_actual}KPH";
+            }
+
+
+            public string EncenderMotor()
+            {
+                if (Encendido == 0)
+                {
+                    Encendido = 1;
+                    return"Encendiendo el auto !!";
+                    return"Empezando a acelerar !!";
+                    velocidad_actual = 0;
+
+                }
+                else
+                {
+                    return"ups el carro ya estaba encendido";
+                }
+            }
+
+
+
+
+
+
+            public string DameInformacion()
+            {
+                return($"Soy un carro marca {Marca} y mi modelo es {Modelo}");
+            }
+
+
+            public string disminuir()
+            {
+
+
+                string mensaje = "";
+
+
+                velocidad_actual -= 15;
+                mensaje = $"vas a {velocidad_actual} KPH";
+
+
+
+
+
+
+                //Console.WriteLine(mensaje);
+                return mensaje;
+
+
+
+            }
+
+            public string Frenado()
+            {
+
+
+                string mensaje = "";
+                if (velocidad_actual <= MAXVELOCIDAD)
+                {
+                    velocidad_actual = 0;
+                    mensaje = $"vas a {velocidad_actual} KPH";
+                }
+                else
+                {
+                    velocidad_actual = MAXVELOCIDAD;
+                    mensaje = "Frenando Auto";
+                }
+
+                //Console.WriteLine(mensaje);
+                return mensaje;
+
+
+
+            }
+
+
+
+
+
+            public string Bocina()
+            {
+
+
+                string mensaje = "";
+
+
+
+                mensaje = "beep!! beeeeep!!";
+
+
+                //Console.WriteLine(mensaje);
+                return mensaje;
+
+
+
+            }
+
+            public string apagado()//funcion de apagado
+            {
+
+
+                string mensaje = "";
+
+
+                if (Encendido == 1)
+                {
+                    Encendido = 0;
+                    mensaje = $"Has apagdo el motor del auto";
+                }
+
+
+                //Console.WriteLine(mensaje);//mensaje
+                return mensaje;
+
+            }
+
+
+            public string marcamodelo()//caracteristicas
+            {
+
+
+                string mensaje = "";
+
+                mensaje = "Caracateristicas del Automovinl: \nMarca: BMW M2 \nModelo: 2022 \nColor: Rojo \nVelocidad Maxima: 250 KP/H";
+
+                //Console.WriteLine(mensaje);//mensaje
+                return mensaje;
+
+
+
+            }
+        }
+
+
+    
+}
---
+++ carro prueba 3/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Text;
+using System.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using carro_prueba_3.Clase;
+
+namespace carro_prueba_3
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Carro carron;
+        public MainWindow()
+        {
+            carron = new Carro("BMW", 2022, "ROJO", 250);
+            InitializeComponent();
+        }
+
+        private void buttonencender_Click(object sender, RoutedEventArgs e)
+        {
+            labelencender.Content = carron.EncenderMotor();
+        }
+
+        private void buttonacelerar_Click(object sender, RoutedEventArgs e)
+        {
+            labelacelerar.Content = carron.acelerar();
+        }
+
+        private void buttonfrenar_Click(object sender, RoutedEventArgs e)
+        {
+            labelfrenar.Content = carron.Frenado();
+        }
+
+        private void buttonbocina_Click(object sender, RoutedEventArgs e)
+        {
+            labelbocina.Content = carron.Bocina();
+        }
+
+        private void ButtonBajarVelocidad_Click(object sender, RoutedEventArgs e)
+        {
+            LabelBajarVelocidad.Content = carron.disminuir();
+        }
+
+        private void butttonApagar_Click(object sender, RoutedEventArgs e)
+        {
+            labelApagar.Content = carron.apagado();
+        }
+
+        private void buttonradio_Click(object sender, RoutedEventArgs e)
+        {
+            SoundPlayer Player = new SoundPlayer();
+            Player.SoundLocation = "C:/Users/Jairo Fernando/Downloads/TIME1.wav";
+            Player.Play();
+        }
+    }
+}
